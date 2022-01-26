d74db8f7fa2b8fbec027a86907de6beaa8fe949d
Oscar Nilsson <oscarandersnilsson@gmail.com>
2022-01-26 16:49:52 +0100

    account settings

--- website/__init__.py
+++ website/__init__.py
@@ -9,10 +9,14 @@ def create_app():
     from .auth import auth
     from .mainpage import main_page
     from .synchronize import synchronize_page
+    from .adddata import add_data
+    from .accountsettings import account_settings
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
     app.register_blueprint(main_page, url_perfix="/")
     app.register_blueprint(synchronize_page, url_perfix="/")
+    app.register_blueprint(add_data, url_prefix="/")
+    app.register_blueprint(account_settings, url_prefix="/")
 
     return app
--- website/auth.py
+++ website/auth.py
@@ -33,8 +33,3 @@ def sign_up():
             flash("Account was created.", category="success")
 
     return render_template("sign_up.html")
-
-
-@auth.route("/add-data", methods=["GET", "POST"])
-def add_data():
-    return render_template("add_data.html")
