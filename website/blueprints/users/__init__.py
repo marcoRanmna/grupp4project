6d575a51ab3ea5de1359bc39daf11b649ef6fa6d
Emcy90 <emma@emcy.se>
2022-02-11 11:41:26 +0100

    diary related actually working now

--- website/blueprints/users/__init__.py
+++ website/blueprints/users/__init__.py
@@ -65,7 +65,8 @@ def diary():
     diary_entry = request.form.get("diary_entry")
     add_diary_note(diary_entry)
     flash("Your diary note has been added!.", category="success")
-    return render_template("diary.html", diary_entry=diary_entry)
+    return redirect(request.referrer)
+    # return render_template("diary.html", diary_entry=diary_entry)
 
 @bp_users.route("/diary", methods=["GET"])
 def diary_get():
