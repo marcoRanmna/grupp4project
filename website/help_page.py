8b976f1986c3b35ebf0de09d175da0cf1b57cc8d
AlexanderAmaechi <Alexander.amaechi@live.com>
2022-02-16 15:47:33 +0100

    Chart branch

--- website/help_page.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-help_page1 = Blueprint("help1", __name__)
-
-
-@help_page1.route("/help-page", methods=["GET", "POST"])
-def help():
-    return render_template("help.html")
